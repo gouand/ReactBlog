051951c909b3672c7aa87401691cf77a230ec684
gouand <48728885+gouand@users.noreply.github.com>
2020-01-16 02:24:01 +0300

    Adding Project

---
+++ src/Posts/Post.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Buttom from '../Button/Button';
+import './Post.css';
+
+
+
+class Post extends React.Component{
+        // eslint-disable-next-line
+    constructor(props){
+        super(props);
+        this.state = { data: localStorage.getItem('localData')}
+        this.removeOnClick = this.removeOnClick.bind(this);
+    }
+
+    removeOnClick(e){
+        this.props.removeOnClick(this.props.id)
+        console.log('Click');
+        console.log(e);
+    }
+    render(){
+        return(
+        <article>
+            <header>
+                <h3>{this.props.title}</h3>
+            </header>
+            <section>
+                <p>{this.props.text}</p>
+            </section>
+            <footer>
+                <div className="tags">
+                    {this.props.tags.map((tag, i) => {
+                        return(<Buttom class="btn btn-xs btn-default" text={tag} key={i}/>)
+                    })
+                    }
+                    
+                </div>
+            </footer>
+            <div className="controls">
+                <button onClick={(e) => this.removeOnClick(e)} className="btn btn-danger btn-mini">удалить</button>
+            </div>
+        </article>
+        )
+        ;
+    }
+}
+
+export default Post;
