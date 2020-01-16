ffa593f7558590ec3b717f3b2cb55e3cb6bdfbe3
gouand <48728885+gouand@users.noreply.github.com>
2020-01-16 13:25:47 +0300

    Adding a build folder in 'build' branch

---
+++ build/precache-manifest.c0017525c3963775593536d011eebc9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc81de7120d297b6cb2934c79bef9b1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b23974258d7e0d9c5e0f",
+    "url": "/static/css/main.b01b446a.chunk.css"
+  },
+  {
+    "revision": "d0da9905cb494fb8d8bd",
+    "url": "/static/js/2.e2e6be41.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e2e6be41.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b23974258d7e0d9c5e0f",
+    "url": "/static/js/main.70f15a88.chunk.js"
+  },
+  {
+    "revision": "f34df4cc19e0da90cba3",
+    "url": "/static/js/runtime-main.578b6095.js"
+  }
+]);
